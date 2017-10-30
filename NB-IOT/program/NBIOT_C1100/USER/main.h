83fdea8a23c184b9c8f8393085d2124f67f97bd5
xuranjob <xuranjob@qq.com>
2017-10-30 14:24:52 +0800

    更改文件名，将农业物联网改为NB-IOT

---
+++ NB-IOT/program/NBIOT_C1100/USER/main.h
@@ -0,0 +1,24 @@
+#ifndef __MAIN_H
+#define __MAIN_H	 
+
+#include "sys.h"
+#include "stm32f10x.h"
+#include "delay.h"
+#include "usart.h" 
+#include "led.h" 		 	     
+#include "usmart.h" 
+#include "malloc.h"
+#include "ff.h"  
+#include "exfuns.h"   
+#include "text.h"	
+#include "touch.h"	
+#include "usart2.h"
+#include "usart3.h"
+#include "sim800c.h" 
+#include "stdarg.h"	 
+#include "stdio.h"	 
+#include "string.h"	 
+#include "timer.h"
+
+#endif
+
